756efa9f8dd37165fa390527010615a527664564
prestonmixon28-del <prestonmixon28@gmail.com>
2026-09-07 16:03:14 -0500

    phase 4 finsih

--- Journal.md
+++ Journal.md
@@ -4,3 +4,5 @@ LIFO  is best for undo because the most recent change can be undone. Always the 
 The undo feature is a stack because stacks use LIFO. 
 
 Having redo uses the second stack to stone the changes that were undone. Whenever I undo a change, it moves the redo stack.
+
+The program checks if the stack is empty before undoing or redoing. This would prevent the program removing something that dosent exist and keeps it from crashing.
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,15 @@ public class Main {
 
        EditorHistory editor = new EditorHistory();
 
-       editor.makeChange("hello");
-       editor.makeChange("hello world");
+       editor.undo();
+       System.out.println(editor.getCurrentState());
+       editor.redo();
+       System.out.println(editor.getCurrentState());
 
+       editor.makeChange("Hello");
+       editor.makeChange("Hello world");
+
+       editor.undo();
        System.out.println(editor.getCurrentState());
        editor.undo();
        System.out.println(editor.getCurrentState());
